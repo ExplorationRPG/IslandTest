668b0e145901aad61f9d27fab1f1fdaab9316889
Ruoyu GP PC <ruoyuw@usc.edu>
2017-12-07 22:28:27 -0800

    add moving platform cpp

---
+++ Source/ExplorationRPG/MovingPlatform.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:629bc0704ccdd3d8951f7ddb79214ea64edec831f3bde34e60d28c12e9b658bb
+size 447
---
+++ Source/ExplorationRPG/MovingPlatform.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:47cb2d741cb6379d084d4907f5d14d24be39ec7eab37549d27a783db151906ae
+size 416
